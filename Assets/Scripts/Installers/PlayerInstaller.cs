ed51b6bdc9a86e29c4f461a03deb7d09c4848760
Shoma SATO <noir.neo.04@gmail.com>
2019-03-23 21:24:52 +0900

    2P input

--- Assets/Scripts/Installers/PlayerInstaller.cs
+++ Assets/Scripts/Installers/PlayerInstaller.cs
@@ -1,11 +1,15 @@
 using Players;
 using Players.InputEventProviderImpls;
+using UnityEngine;
 using Zenject;
 
 public class PlayerInstaller : MonoInstaller<PlayerInstaller>
 {
+    [SerializeField]
+    int playerNum;
+
     public override void InstallBindings()
     {
-        Container.Bind<IInputEventProvider>().To<PlayerInputEventProvider>().AsSingle();
+        Container.Bind<IInputEventProvider>().To<PlayerInputEventProvider>().AsSingle().WithArguments(playerNum);
     }
 }
