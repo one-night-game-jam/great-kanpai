5439850aef56755fcd10683ec59c35fcadca64e7
Shoma SATO <noir.neo.04@gmail.com>
2019-03-23 15:01:24 +0900

    Add PlayerCore skeleton

---
+++ Assets/Scripts/Players/PlayerCore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+namespace Players
+{
+    public class PlayerCore : MonoBehaviour
+    {
+        public IObservable<Unit> OnJump { get; }
+        public IObservable<bool> OnMove { get; }
+        public IObservable<Unit> OnHit { get; }
+        public IObservable<Unit> OnDied { get; }
+    }
+}
