9322160065862523950c595d05a9c1e72568abe8
Shoma SATO <noir.neo.04@gmail.com>
2019-03-23 17:37:21 +0900

    Emmit kanpai se

---
+++ Assets/Scripts/Players/SeEmitter.cs
@@ -0,0 +1,22 @@
+﻿using UniRx;
+using UnityEngine;
+using Zenject;
+
+namespace Players
+{
+    public class SeEmitter : MonoBehaviour
+    {
+        [SerializeField]
+        AudioSource hit;
+
+        [Inject]
+        PlayerCore core;
+
+        void Start()
+        {
+            core.OnHit
+                .Subscribe(_ => hit.Play())
+                .AddTo(this);
+        }
+    }
+}
