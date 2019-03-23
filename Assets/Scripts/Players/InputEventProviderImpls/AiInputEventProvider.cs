f0a2a7b569b6f37f39426b133dbde88f0d1c33cf
Shoma SATO <noir.neo.04@gmail.com>
2019-03-23 22:57:00 +0900

    Add Ai input

---
+++ Assets/Scripts/Players/InputEventProviderImpls/AiInputEventProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UniRx;
+
+namespace Players.InputEventProviderImpls
+{
+    public class AiInputEventProvider : IInputEventProvider, IDisposable
+    {
+        readonly ReactiveProperty<bool> jump = new ReactiveProperty<bool>();
+        readonly ReactiveProperty<float> move = new ReactiveProperty<float>();
+        readonly CompositeDisposable disposable = new CompositeDisposable();
+
+        IReadOnlyReactiveProperty<bool> IInputEventProvider.Jump => jump;
+        IReadOnlyReactiveProperty<float> IInputEventProvider.Move => move;
+
+        AiInputEventProvider()
+        {
+            
+        }
+
+        void IDisposable.Dispose()
+        {
+            disposable.Dispose();
+        }
+    }
+}
