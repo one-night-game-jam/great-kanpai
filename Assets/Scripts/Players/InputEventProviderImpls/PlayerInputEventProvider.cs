bb5101873564b4fe9052fa0db1904ec22390c905
Shoma SATO <noir.neo.04@gmail.com>
2019-03-23 15:30:49 +0900

    Implement player input

---
+++ Assets/Scripts/Players/InputEventProviderImpls/PlayerInputEventProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+namespace Players.InputEventProviderImpls
+{
+    public class PlayerInputEventProvider : IInputEventProvider, IDisposable
+    {
+        readonly ReactiveProperty<bool> jump = new ReactiveProperty<bool>();
+        readonly ReactiveProperty<float> move = new ReactiveProperty<float>();
+        readonly CompositeDisposable disposable = new CompositeDisposable();
+
+        IReadOnlyReactiveProperty<bool> IInputEventProvider.Jump => jump;
+        IReadOnlyReactiveProperty<float> IInputEventProvider.Move => move;
+
+        PlayerInputEventProvider(int playerNum = 0)
+        {
+            this.ObserveEveryValueChanged(_ => Input.GetButton($"Player{playerNum}Jump"))
+                .Subscribe(x => jump.Value = x)
+                .AddTo(disposable);
+            this.ObserveEveryValueChanged(_ => Input.GetAxis($"Player{playerNum}Horizontal"))
+                .Subscribe(x => move.Value = x)
+                .AddTo(disposable);
+        }
+
+        public void Dispose()
+        {
+            disposable.Dispose();
+        }
+    }
+}
