75796d4e2dee544eb1e64bbe656f9685c83fe07e
Shoma SATO <noir.neo.04@gmail.com>
2019-03-23 15:02:00 +0900

    Modify namespace

--- Assets/Scripts/Players/PlayerMover.cs
+++ Assets/Scripts/Players/PlayerMover.cs
@@ -2,26 +2,29 @@
 using UniRx.Triggers;
 using UnityEngine;
 
-public class PlayerMover : MonoBehaviour
+namespace Players
 {
-    [SerializeField]
-    Rigidbody rb;
-    [SerializeField]
-    float movePower;
-    [SerializeField]
-    float jumpPower;
-
-    void Start()
+    public class PlayerMover : MonoBehaviour
     {
-        this.UpdateAsObservable()
-            .Where(_ => Input.GetKeyDown("space"))
-            .ObserveOn(Scheduler.MainThreadFixedUpdate)
-            .Subscribe(_ => rb.AddForce(0, jumpPower, 0, ForceMode.Impulse))
-            .AddTo(this);
-    }
+        [SerializeField]
+        Rigidbody rb;
+        [SerializeField]
+        float movePower;
+        [SerializeField]
+        float jumpPower;
 
-    void FixedUpdate()
-    {
-        rb.AddForce(movePower, 0, 0, ForceMode.Acceleration);
+        void Start()
+        {
+            this.UpdateAsObservable()
+                .Where(_ => Input.GetKeyDown("space"))
+                .ObserveOn(Scheduler.MainThreadFixedUpdate)
+                .Subscribe(_ => rb.AddForce(0, jumpPower, 0, ForceMode.Impulse))
+                .AddTo(this);
+        }
+
+        void FixedUpdate()
+        {
+            rb.AddForce(movePower, 0, 0, ForceMode.Acceleration);
+        }
     }
 }
